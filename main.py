ab8c22deb18cb34525b48a1cbcc05589bfca42f4
Brian Seo <98533321+BrianYSeo@users.noreply.github.com>
2022-04-05 01:05:49 +0000

    Completed Midterm

--- main.py
+++ main.py
@@ -6,34 +6,44 @@ import math
 # then you do not fully understand functions
 # and should review how they work or ask for help
 def drawSineCurve(dart):
+  dart.up()
+  dart.goto(-360,0)
+  dart.down()
   for i in range(-360,360):
-    y=math.sin(math.radians(i))
-    dart.goto(i,y)
+    yvar = math.sin(math.radians(i))
+    dart.goto(i,yvar)
 def setupWindow(window):
   window.setworldcoordinates(-360, -1, 360, 1)
 def setupAxis(dart):
   for i in range(4):
+    dart.up()
     dart.goto(0,0)
     dart.down()
     dart.forward(360)
     dart.left(90)
-    dart.up()
-  dart.goto(-360, 0)
-  dart.down()
 def drawCosineCurve(dart):
   dart.up()
   dart.goto(-360, 1)
   dart.down()
   for i in range(-360, 360):
-    y = math.cos(math.radians(i))
-    dart.goto(i,y)
+    yvar = math.cos(math.radians(i))
+    dart.goto(i,yvar)
 def drawTangentCurve(dart):
   dart.up()
   dart.goto(-360,0)
   dart.down()
   for i in range (-360, 360):
-    y = math.tan(math.radians(i))
-    dart.goto(i,y)
+    yvar = math.tan(math.radians(i))
+    dart.goto(i,yvar)
+def drawCotangentCurve(window,dart):
+  window.setworldcoordinates(-1,-1,1,1)
+  dart.up()
+  dart.goto(360, 0)
+  dart.down()
+  dart.color("red");
+  for i in range(0,360):
+    dart.goto(math.cos(math.radians(i)), math.sin(math.radians(i)))
+    
   
 ##########  Do Not Alter Any Code Past Here ########
 def main():
@@ -48,8 +58,14 @@ def main():
     setupWindow(wn)
     setupAxis(dart)
     dart.speed(0)
-    drawSineCurve(dart)
-    drawCosineCurve(dart)
-    drawTangentCurve(dart)
+    curve = input("Which curve do you want to draw? (Choose one: sin, cos, tan, circle)")
+    if (curve == "sin"):
+      drawSineCurve(dart)
+    if (curve == "cos"):
+      drawCosineCurve(dart)
+    if (curve == "tan"):
+      drawTangentCurve(dart)
+    if (curve == "circle"):
+      drawCotangentCurve(wn,dart)
     wn.exitonclick()
 main()
